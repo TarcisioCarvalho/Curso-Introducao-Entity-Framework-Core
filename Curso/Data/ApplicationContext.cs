577007cf08b3f900874eb0d412db13b8fa06d8c6
tarcisiokta2012@gmail.com <tarcisiokta2012@gmail.com>
2022-10-19 00:24:59 -0300

    Finish basic course Entity Frameworkcore

--- Curso/Data/ApplicationContext.cs
+++ Curso/Data/ApplicationContext.cs
@@ -1,21 +1,33 @@
 using Microsoft.EntityFrameworkCore;
 using CursoEFCore.Domain;
 using Microsoft.EntityFrameworkCore.Design;
+using CursoEFCore.Data.Configurations;
+using Microsoft.Extensions.Logging;
 
 namespace CursoEFCore.Data
 {
     public class ApplicationContext:DbContext
     {
+        private static readonly ILoggerFactory _logger = LoggerFactory.Create(p => p.AddConsole());
         public DbSet<Pedido> Pedidos { get; set; }
-
+        public DbSet<Produto> Produtos { get; set; }
+        public DbSet<Cliente> Clientes { get; set; }
+        
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            optionsBuilder.UseSqlServer("Data source=(localdb)\\mssqllocaldb; Initial Catalog=CursoEFCore; Integrated Security = true");
+            optionsBuilder
+            .UseLoggerFactory(_logger)
+            .EnableSensitiveDataLogging()
+            .UseSqlServer("Data source=(localdb)\\mssqllocaldb; Initial Catalog=CursoEFCore; Integrated Security=true");
         }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
-            modelBuilder.ApplyConfigurationsFromAssembly(typeof(ApplicationContext).Assembly);
+            //modelBuilder.ApplyConfigurationsFromAssembly(typeof(ApplicationContext).Assembly);
+            modelBuilder.ApplyConfiguration(new ClienteConfiguration());
+            modelBuilder.ApplyConfiguration(new PedidoConfiguration());
+            modelBuilder.ApplyConfiguration(new ProdutoConfiguration());
+            modelBuilder.ApplyConfiguration(new PedidoItemConfiguration());
         }
     }
 }
--- Curso/Program.cs
+++ Curso/Program.cs
@@ -1,4 +1,9 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using CursoEFCore.Domain;
+using CursoEFCore.ValueObjects;
+using Microsoft.EntityFrameworkCore;
 
 namespace CursoEFCore
 {
@@ -6,7 +11,103 @@ namespace CursoEFCore
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //InserirDados();
+            //InserirDadosEmMassa();
+            //CadastrarPedido();
+            //ConsultarPedidoCarregamentoAdiantado();
+            AtualizarDados();
+        }
+
+        private static void AtualizarDados()
+        {
+            using var db = new Data.ApplicationContext();
+            var cliente = db.Clientes.Find(1);
+            cliente.Nome = "Nome Alterado";
+
+            db.SaveChanges();
+        }
+        private static void ConsultarPedidoCarregamentoAdiantado()
+        {
+            using var db = new Data.ApplicationContext();
+            var pedidos = db
+            .Pedidos
+            .Include(p => p.Items)
+            .ThenInclude(p => p.Produto)
+            .ToList();
+
+            System.Console.WriteLine(pedidos.Count);
+        }
+        private static void CadastrarPedido()
+        {
+            using var db = new Data.ApplicationContext();
+            var cliente = db.Clientes.FirstOrDefault();
+            var produto = db.Produtos.FirstOrDefault();
+
+            var pedido = new Pedido 
+            {
+                ClienteId = cliente.Id,
+                IniciadoEm = DateTime.Now,
+                FinalizadoEm = DateTime.Now,
+                Observacao = "Pedido Teste",
+                Status = StatusPedido.Analise,
+                TipoFrete = TipoFrete.SemFrete,
+                Items = new List<PedidoItem> 
+                {
+                    new PedidoItem 
+                    {
+                        ProdutoId = produto.Id,
+                        Desconto = 0,
+                        Quantidade = 1,
+                        Valor = 10,
+                    }
+                }
+            };
+            db.Pedidos.Add(pedido);
+            db.SaveChanges();
+        }
+
+        private static void InserirDadosEmMassa()
+        {
+              Produto produto = new Produto
+            {
+                Descricao = "Primeiro Teste",
+                CodigoBarras = "1234567891232",
+                Valor = 10m,
+                TipoProduto = TipoProduto.MercadoriaParaRevenda,
+                Ativo = true
+            };
+            
+                Cliente cliente = new Cliente
+            {
+                Nome = "Jeferson Jubileu",
+                Cep = "99999000",
+                Cidade = "Erechin",
+                Estado = "RS",
+                Telefone = "99000000001"
+            };
+
+            using var db = new Data.ApplicationContext();
+            db.AddRange(produto,cliente);
+            var registros = db.SaveChanges();
+            System.Console.WriteLine($"O total de Registros {registros}");
+        }
+
+        private static void InserirDados()
+        {
+            Produto produto = new Produto
+            {
+                Descricao = "Primeiro Teste",
+                CodigoBarras = "1234567891232",
+                Valor = 10m,
+                TipoProduto = TipoProduto.MercadoriaParaRevenda,
+                Ativo = true
+            };
+
+            using var db = new Data.ApplicationContext();
+            db.Produtos.Add(produto);
+
+            var registros = db.SaveChanges();
+            Console.WriteLine($"Total de Registros {registros}");
         }
     }
 }
